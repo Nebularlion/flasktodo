89a76948854922f44e440bf403f5f3f20caad1d8
Nebularlion <elina.rajala@hotmail.fi>
2018-12-11 00:11:58 +0200

    Some bootstrap

--- flasktodo/db.py
+++ flasktodo/db.py
@@ -1,14 +1,16 @@
 import sqlite3
 
 import click
-from flask import current_app, g, 
-from flask_sqlalchemy import SQLAlchemy
+from flask import current_app, g
 from flask.cli import with_appcontext
 
 
 def get_db():
     if 'db' not in g:
-        g.db = SQLAlchemy(current_app)
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
         g.db.row_factory = sqlite3.Row
 
     return g.db
@@ -39,4 +41,3 @@ def init_db_command():
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
-
